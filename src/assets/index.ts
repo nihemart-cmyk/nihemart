4913a45997be50f1cb0f6faf0530eea6fd5e48d6
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-10-03 19:55:35 +0200

    feat: change categories layout

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,10 @@
 export {default as profilePlaceholder } from './imgs/profile-placeholder.png'
 export  {default as slideImg1} from './imgs/landingpage/slide.png'
-export  {default as carousel1} from './carousel-imgs/1.jpg'
-export  {default as carousel2} from './carousel-imgs/2.jpg'
+export  {default as carousel1} from './carousel-imgs/1.png'
+export  {default as carousel2} from './carousel-imgs/2.png'
 export  {default as carousel3} from './carousel-imgs/3.png'
 export  {default as carousel4} from './carousel-imgs/4.png'
+export  {default as carousel5} from './carousel-imgs/5.jpg'
 
 
 // Icons
