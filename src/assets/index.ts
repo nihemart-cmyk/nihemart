bde8481aa6b4c72432058a1b633dbe6803981820
FADHILI Josue <fadhilijosue27@gmail.com>
2025-10-09 14:30:18 +0200

    feat: compressed images for faster loading

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,9 @@
 export {default as profilePlaceholder } from './imgs/profile-placeholder.png'
-export  {default as carousel1} from './carousel-imgs/compressed-1.webp'
-export  {default as carousel2} from './carousel-imgs/compressed-2.webp'
-export  {default as carousel3} from './carousel-imgs/3.png'
-export  {default as carousel4} from './carousel-imgs/4.png'
-export  {default as carousel5} from './carousel-imgs/5.jpg'
+export  {default as carousel1} from './carousel-imgs/1-compressed.webp'
+export  {default as carousel2} from './carousel-imgs/2-compressed.webp'
+export  {default as carousel3} from './carousel-imgs/3-compressed.webp'
+export  {default as carousel4} from './carousel-imgs/4-compressed.webp'
+export  {default as carousel5} from './carousel-imgs/5-compressed.webp'
 
 
 // Icons
