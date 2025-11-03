86ad015753c3e82e2f8bdd5a76643251676f1aa3
FADHILI Josue <fadhilijosue27@gmail.com>
2025-11-03 22:46:55 +0200

    refactor: refactor image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,6 +4,7 @@ export { default as carousel2 } from "./carousel-imgs/2.webp";
 export { default as carousel3 } from "./carousel-imgs/3.webp";
 export { default as carousel4 } from "./carousel-imgs/4.webp";
 export { default as carousel5 } from "./carousel-imgs/5.jpg";
+export { default as aboutImg } from './imgs/about.jpg'
 
 // Icons
 export { default as momoIcon } from "./icons/mtn.png";
