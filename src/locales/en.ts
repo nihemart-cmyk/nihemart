39a32f673f3e08d40d61a447e323569c58511268
bertin <iradukundabertin082@gmail.com>
2025-11-21 09:52:54 +0200

    fix: checkout translations

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -209,6 +209,7 @@ export const en: Record<string, string> = {
    "checkout.loginToPlaceOrder": "Please log in to place an order",
    "checkout.loginToContinue": "Login to Continue",
    "checkout.missing.address": "Please fill in your address",
+   "checkout.missing.phone": "Please provide a valid phone number",
    "checkout.missing.header":
       "Please fill the following before placing an order:",
    "checkout.errors.validPhone": "Enter a valid phone number (10 digits)",
